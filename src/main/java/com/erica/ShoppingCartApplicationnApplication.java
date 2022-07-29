d583ca4c0a7c10b06e0d0843d57a902f0f6d3ca4
marcelocardosodev <cardosom2003dev@gmail.com>
2022-07-28 23:35:01 -0300

    Criada estrutura do inicial do projeto

---
+++ src/main/java/com/erica/ShoppingCartApplicationnApplication.java
@@ -0,0 +1,13 @@
+package com.erica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplicationnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplicationnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erica/ShoppingCartApplicationnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
